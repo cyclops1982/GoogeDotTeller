6da21185f3e0904cade71d813b51b2e18d8cef22
Ruben D’Arco <Ruben.DArco@Nintex.com>
2022-08-22 08:48:06 -0700

    Simple static version

--- GoogeCounter/Program.cs
+++ GoogeCounter/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SharpPcap;
+
+namespace GoogeCounter
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var ver = Pcap.SharpPcapVersion;
+            Console.WriteLine("SharpPcap {0}, Example5.PcapFilter.cs\n", ver);
+
+            // Retrieve the device list
+            var devices = CaptureDeviceList.Instance;
+
+            // If no devices were found print an error
+            if (devices.Count < 1)
+            {
+                Console.WriteLine("No devices were found on this machine");
+                return;
+            }
+
+            Console.WriteLine("The following devices are available on this machine:");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine();
+
+            int i = 0;
+
+            // Scan the list printing every entry
+            foreach (var dev in devices)
+            {
+                Console.WriteLine("{0}) {1}", i, dev.Description);
+                i++;
+            }
+
+            Console.WriteLine();
+            Console.Write("-- Please choose a device to capture: ");
+            i = int.Parse(Console.ReadLine());
+
+            using var device = devices[i];
+
+            //Register our handler function to the 'packet arrival' event
+            device.OnPacketArrival +=
+                new PacketArrivalEventHandler(device_OnPacketArrival);
+
+            //Open the device for capturing
+            int readTimeoutMilliseconds = 1000;
+            device.Open(DeviceModes.Promiscuous, readTimeoutMilliseconds);
+
+            // tcpdump filter to capture only TCP/IP packets
+            string filter = "dst net 8.8.4.0/24 or dst net 8.8.8.0/24";
+            device.Filter = filter;
+
+            Console.WriteLine();
+            Console.WriteLine
+                ("-- The following tcpdump filter will be applied: \"{0}\"",
+                filter);
+            Console.WriteLine
+                ("-- Listening on {0}, hit 'Ctrl-C' to exit...",
+                device.Description);
+
+            // Start capture packets
+            device.Capture();
+
+        }
+
+        /// <summary>
+        /// Prints the time and length of each received packet
+        /// </summary>
+        private static void device_OnPacketArrival(object sender, PacketCapture e)
+        {
+            var time = e.Header.Timeval.Date;
+            var len = e.Data.Length;
+            Console.WriteLine("{0}:{1}:{2},{3} Len={4}",
+                time.Hour, time.Minute, time.Second, time.Millisecond, len);
+            Console.Beep();
+        }
+    }
+
+}
